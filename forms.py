7978ad3efac89c4971f282fed2a3fe19b34af1b1
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-02-06 05:12:10 +0300

    SignUp & SignIn form classes

---
+++ forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length,  Email, EqualTo
+
+class SignUpForm(FlaskForm):
+    """A signup form to signup for registering
+    users on the site
+    """
+    first_name = StringField('First Name',
+                             validators=[DataRequired()])
+    last_name = StringField('Last Name',
+                            validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password',
+                             validators=[DataRequired(), EqualTo('password')])
+    
+    sign_up = SubmitField('Sign Up')
+
+
+
+class SignInForm(FlaskForm):
+    """A signIn form to log user in on our site"""
+
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    remember_me  = BooleanField('Remember Me')
+    sign_in = SubmitField('Sign In')
+
+
+
